2a44c068ff209eb3d79ade9017121abe02a919fa
SPavel91 <slavin.pavel1991@gmail.com>
2024-09-22 15:57:30 +0700

    22/09

---
+++ variables.py
@@ -0,0 +1,5 @@
+Number_of_homework_completed = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_per_task = Number_of_hours_spent / Number_of_homework_completed
+print('Курс:'+Course_name+', всего задач:'+str(Number_of_homework_completed)+', затрачено часов:'+str(Number_of_hours_spent)+', среднее время выполнения',Time_per_task, 'часа.')
