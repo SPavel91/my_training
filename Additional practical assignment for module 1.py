18b48761f7da7b1ff05ae89ea9ba0b0ab155a2f8
SPavel91 <slavin.pavel1991@gmail.com>
2024-09-27 19:20:24 +0700

    initial

--- Additional practical assignment for module 1.py
+++ Additional practical assignment for module 1.py
@@ -1,7 +1,6 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-students_l = list(students)
-students_l.sort()
+students_l = sorted(list(students))
 all_students = {students_l[0] : float(sum(grades[0]) / len(grades[0])),
                 students_l[1] : float(sum(grades[1]) / len(grades[1])),
                 students_l[2] : float(sum(grades[2]) / len(grades[2])),
