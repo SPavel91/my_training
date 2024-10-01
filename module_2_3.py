e72e1e9054a6fdb67cc6b1e809070e6dd8b968cf
SPavel91 <slavin.pavel1991@gmail.com>
2024-10-02 02:24:23 +0700

    initial

--- module_2_3.py
+++ module_2_3.py
@@ -2,7 +2,7 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 a = 0
 while a < len(my_list):
    if my_list[a] > 0:
-    print(my_list[a])
+       print(my_list[a])
    a += 1
    if my_list[a] < 0:
-        break
+       break
