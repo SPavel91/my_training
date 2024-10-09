4e7cb1a9f1583ed916fc50e7ba0c4f5a7e6ba751
SPavel91 <slavin.pavel1991@gmail.com>
2024-10-09 19:55:44 +0700

    initial

--- module__2_hard_1.0.py
+++ module__2_hard_1.0.py
@@ -2,19 +2,15 @@ result = ''
 n = int(input("Введите число от 3 до 20: ",))
 z = 0
 x = 1
-while n < 3:
+
+while not 2 < n < 21:
     print('Вы ввели не верное число, по пробуйте еще раз!')
     n = int(input())
     continue
 
-while n > 20:
-    print('Вы ввели не верное число, по пробуйте еще раз!')
-    n = int(input())
-    continue
-
-while 2 < n < 21:
+while 2 < n:
     z += 1
-    while 2 < n < 21:
+    while 2 < n:
         x += 1
         if n % (z + x) == 0:
             result += str(z) + str(x)
