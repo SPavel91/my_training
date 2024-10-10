be0f3038ef79aff79b1b796dc7e57325ff958408
SPavel91 <slavin.pavel1991@gmail.com>
2024-10-10 17:33:28 +0700

    initial

--- module_3_2.py
+++ module_3_2.py
@@ -1,7 +1,7 @@
 com = "moc."
 ru = "ur."
 net = "ten."
-def send_email(message, recipient, sender = "university.help@gmail.com"):
+def send_email(message, recipient, *,sender = "university.help@gmail.com"):
     m = 0
     q = 0
     w = 0
@@ -43,7 +43,7 @@ def send_email(message, recipient, sender = "university.help@gmail.com"):
             print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
 
