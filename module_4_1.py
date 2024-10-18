3d62bb464005de36c30fe713fdb6e6d6bea1f622
SPavel91 <slavin.pavel1991@gmail.com>
2024-10-18 17:26:02 +0700

    initial

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import  divide as divide_1
+from true_math import  divide as divide_2
+
+result1 = divide_1(69, 3)
+result2 = divide_1(3, 0)
+result3 = divide_2(49, 7)
+result4 = divide_2(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
