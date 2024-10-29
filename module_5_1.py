9216755dfb4a76c6093c0b51663ae31dd33fef59
SPavel91 <slavin.pavel1991@gmail.com>
2024-10-29 13:11:20 +0700

    initial

---
+++ module_5_1.py
@@ -0,0 +1,23 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.new_floor = None
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if 1 <= self.new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print('"Такого этажа не существует"')
+
+
+h1 = House('ЖК Горский', 18)
+
+h2 = House('Домик в деревне', 2)
+
+h1.go_to(5)
+
+h2.go_to(10)
