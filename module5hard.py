32e7037ed0f4745872fde2208c9a114f324ef462
SPavel91 <slavin.pavel1991@gmail.com>
2024-11-07 09:34:26 +0700

    initial

--- module5hard.py
+++ module5hard.py
@@ -37,50 +37,53 @@ class UrTube:
     def register(self, nickname, password, age):
         for user in self.users:
             if user.nickname == nickname:
-                print(f'"Пользователь {nickname} уже существует".')
+                print(f'Пользователь {nickname} уже существует.')
                 return
         new_user = User(nickname, password, age)
         self.users.append(new_user)
         self.current_user = new_user
-        print(f'Пользователь {nickname} зарегистрирован.')
 
     def log_out(self):
         self.current_user = None
 
-
     def add(self, *videos):
         for video in videos:
-            if video.title not in [v.title for v in self.videos]:  # Проверяем существование по названию
+            if video not in self.videos:
                 self.videos.append(video)
-
-
-    def watch_video(self, title):
-        for video in self.videos:
-            if video.title == title:
-                for i in range(1, video.duration + 1):
-                    print(i)
-                    time.sleep(1)
-                return
-        print("Видео нет.")
 
     def get_videos(self, keyword):
         result = []
-        for video in Video.lib_videos:
+        for video in self.videos:
             if keyword.lower() in video.title.lower():
-                result.append(video)
+                result.append(video.title)
         return result
 
+    def watch_video(self, title):
+        if self.current_user is None:
+            print("Войдите в аккаунт, чтобы смотреть видео")
+            return
+        for video in self.videos:
+            if video.title == title:
+                if video.adult_mode and self.current_user.age < 18:
+                    print("Вам нет 18 лет, пожалуйста покиньте страницу")
+                    return
+                for i in range(1, video.duration + 1):
+                    print(i)
+                    time.sleep(1)
+                print("Конец видео.")
+                self.current_user = self.current_user.nickname
 
-#if __name__ == '__main__':
+
+
 
 ur = UrTube()
 v1 = Video('Лучший язык программирования 2024 года', 200)
 v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
-#
+
 # Добавление видео
 ur.add(v1, v2)
-#
-# # Проверка поиска
+
+# Проверка поиска
 print(ur.get_videos('лучший'))
 print(ur.get_videos('ПРОГ'))
 
@@ -90,11 +93,10 @@ ur.register('vasya_pupkin', 'lolkekcheburek', 13)
 ur.watch_video('Для чего девушкам парень программист?')
 ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
 ur.watch_video('Для чего девушкам парень программист?')
-#
+
 # Проверка входа в другой аккаунт
 ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
 print(ur.current_user)
 
 # Попытка воспроизведения несуществующего видео
-
 ur.watch_video('Лучший язык программирования 2024 года!')
