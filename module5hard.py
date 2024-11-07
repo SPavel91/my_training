ce41e1f5f9dfb78f2b8fba51c92f5d628fe3a06b
SPavel91 <slavin.pavel1991@gmail.com>
2024-11-07 09:38:30 +0700

    initial

--- module5hard.py
+++ module5hard.py
@@ -60,15 +60,15 @@ class UrTube:
 
     def watch_video(self, title):
         if self.current_user is None:
-            print("Войдите в аккаунт, чтобы смотреть видео")
+            print("Войдите в аккаунт, чтобы смотреть видео.")
             return
         for video in self.videos:
             if video.title == title:
                 if video.adult_mode and self.current_user.age < 18:
-                    print("Вам нет 18 лет, пожалуйста покиньте страницу")
+                    print("Вам нет 18 лет, пожалуйста покиньте страницу.")
                     return
                 for i in range(1, video.duration + 1):
-                    print(i)
+                    print(i, end=" ")
                     time.sleep(1)
                 print("Конец видео.")
                 self.current_user = self.current_user.nickname
