a2ab3c83e5e543ba538171bbad4ce5735d38e6da
SPavel91 <slavin.pavel1991@gmail.com>
2024-11-18 21:20:50 +0700

    initial

--- 7_module_Formatting a string.py
+++ 7_module_Formatting a string.py
@@ -6,15 +6,18 @@ team1_time = 1552.512
 team2_time = 2153.31451
 tasks_total = score_1 + score_2
 time_avg = (team1_time + team2_time) / tasks_total
-challenge_result = 'Победа команды Волшебники данных!'
+challenge_result = ""
 print("В команде Мастера кода участников: %s!" % team1_num)
 print("Итого сегодня в командах участников: %s и %s!" % (team1_num, team2_num))
 print("Команда Волшебники данных решила задач: {}!".format(score_2))
 print("Волшебники данных решили задачи за {}c.!".format(team2_time))
 print(f"Команды решили {score_1} и {score_2} задач.")
-if challenge_result:
+if challenge_result == "":
     if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
-        print("Победа команды Мастера кода!")
+        challenge_result = "Победа команды Мастера кода!"
     elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
-        print("Победа команды Волшебники Данных!")
+        challenge_result = "Победа команды Волшебники Данных!"
+else:
+    challenge_result = "Ничья!"
+print(challenge_result)
 print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!")
