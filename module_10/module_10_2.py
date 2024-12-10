b1d2d1901fc1068f5d62dfd8d7decac29c0acb49
SPavel91 <slavin.pavel1991@gmail.com>
2024-12-10 18:36:26 +0700

    initial

--- module_10/module_10_2.py
+++ module_10/module_10_2.py
@@ -15,13 +15,12 @@ class Knight(threading.Thread):
         print(f'{self.name} на нас напали!')
         enemies = 100
         days = 0
-        while enemies != 0:
+        while enemies > 0:
             enemies -= self.power
             days += 1
-            print(f'{self.name}, сражается {days} день(дня)..., осталось {enemies} воинов')
+            print(f'{self.name}, сражается {days} день(дня)..., осталось {max(enemies, 0)} воинов')
             sleep(1)
         print(f'{self.name} одержал победу спустя {days} дней(дня)!')
-
 
 
 first_knight = Knight('Sir Lancelot', 10)
