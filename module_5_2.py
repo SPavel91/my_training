3198bbc8e2a75c89dee6ef3b8a3bca10dcc3cff3
SPavel91 <slavin.pavel1991@gmail.com>
2024-12-16 20:31:46 +0700

    initial

--- module_5_2.py
+++ module_5_2.py
@@ -22,19 +22,16 @@ class House:
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
 
-
-
-
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
-# h1.go_to(5)
-# h2.go_to(10)
+h1.go_to(5)
+h2.go_to(10)
 # __str__
 print(h1)
 print(h2)
 
 # __len__
-#print(len(h1))
-#print(len(h2))
+print(len(h1))
+print(len(h2))
 
 
