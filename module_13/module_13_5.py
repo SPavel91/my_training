0595599779685de1000e3e95e3f11c5a1899c834
SPavel91 <slavin.pavel1991@gmail.com>
2025-01-16 12:19:28 +0700

    initial

---
+++ module_13/module_13_5.py
@@ -0,0 +1,66 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+api = ""
+
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage = MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard = True)
+button1 = KeyboardButton(text="Рассчитать")
+button2 = KeyboardButton(text="Информация")
+kb.add(button1)
+kb.add(button2)
+
+
+@dp.message_handler(commands=["start"])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup = kb)
+    await message.answer('Хотите рассчитать свою норму калорий в день?')
+
+
+@dp.message_handler(text="Информация")
+async def information(message):
+    await message.answer('Пока нет информации')
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text="Рассчитать")
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    await message.answer(f'Средняя норма калорий в день:'
+                         f' {10 * float(data["weight"]) + 6.25 * float(data["growth"]) - 5 * float(data["age"]) - 161}')
+    await state.finish()
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates = True)
