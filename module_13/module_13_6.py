83319a5a7191b509814d2b5f6319a05b3a8549e5
SPavel91 <slavin.pavel1991@gmail.com>
2025-01-18 14:06:08 +0700

    initial

--- module_13/module_13_6.py
+++ module_13/module_13_6.py
@@ -5,7 +5,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import asyncio
 
-api = "7650152262:AAEQ3FBcy7IDevTgaHUHTjnpdtTG0daKx8E"
+api = ""
 
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
